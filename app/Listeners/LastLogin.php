fa244d730d1ba4b484ce96a2345614613debbbb1
cuongnd1705 <cuongnd.work@gmail.com>
2021-06-12 17:21:04 +0700

    logout listeners

---
+++ app/Listeners/LastLogin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Carbon\Carbon;
+use Illuminate\Auth\Events\Logout;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class LastLogin
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param Logout $event
+     * @return void
+     */
+    public function handle(Logout $event)
+    {
+        $user = $event->user;
+        $user->last_login = Carbon::now()->toDateTimeString();
+        $user->save();
+    }
+}
---
+++ resources/views/user/pages/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.user')
+
+@section('title', 'Trang chủ')
+
+@section('css-libraries')
+@endsection
+
+@section('css')
+@endsection
+
+@section('content')
+    123
+@endsection
+
+@section('js-libraries')
+@endsection
+
+@section('js')
+@endsection
